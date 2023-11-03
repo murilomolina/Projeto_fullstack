37d09d55ad321f2ee652600cfef9968bf098d5d6
Murilo Molina <murilo.m.barone@gmail.com>
2023-11-03 12:44:06 -0300

    proximo passo instalar o mongodb via npm install

--- front/js/script.js
+++ front/js/script.js
@@ -16,5 +16,36 @@ async function obterFilmes(){
         celulaTitulo.innerHTML = filme.titulo;
         celulaSinopse.innerHTML = filme.sinopse;
     }
+}
+async function cadastrarFilme(){
+    const URLcompleta = `${protocolo}${baseUrl}${filmesEndPoint}`;
+    let tituloInput = document.querySelector('#tituloInput');
+    let sinopseInput = document.querySelector('#sinopseInput');
+    let titulo = tituloInput.value;
+    let sinopse = sinopseInput.value;
+    if (titulo && sinopse){
+    tituloInput.value = '';
+    sinopseInput.value = '';
+    const filmes = (await axios.post(URLcompleta, {titulo, sinopse})).data;
+    let tabela = document.querySelector('.filmes');
+    let corpoTabela = tabela.getElementsByTagName('tbody')[0];
+    corpoTabela.innerHTML = '';
 
+    for (let filme of filmes) {
+        let linha = corpoTabela.insertRow(0);
+        let celulaTitulo = linha.insertCell(0);
+        let celulaSinopse = linha.insertCell(1);
+        celulaTitulo.innerHTML = filme.titulo;
+        celulaSinopse.innerHTML = filme.sinopse;
+    }
+    }
+    else {
+        let alert = document.querySelector('.alert');
+        alert.classList.add('show');
+        alert.classList.remove('d-none');
+        setTimeout(() => {
+            alert.classList.add('d-none');
+            alert.classList.remove('show');
+        }, 5000);
+    }
 }
